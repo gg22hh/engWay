d9a876bfa2ccc1c8dedaaaf97ddddd6e044a576c
g322h <g2203h@gmail.com>
2022-04-01 22:06:45 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./App.css";
 import { LoginForm } from "./components/LoginForm/LoginForm";
-import { Route, Routes } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { Home } from './pages/Home/Home';
-import {Listening} from './pages/Listening/Listening'
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home/Home";
+import { Listening } from "./pages/Listening/Listening";
 import { Reading } from "./pages/Reading/Reading";
 import { Grammar } from "./pages/Grammar/Grammar";
 
 function App() {
-	const [activeLoginForm, setActiveLoginForm] = useState(false)
-	const [darkTheme, setDarkTheme] = useState(
-		localStorage.getItem('theme') === 'true' 
-	)
-	const [userName, setUserName] = useState('')
-
+    const [activeLoginForm, setActiveLoginForm] = useState(false);
+    const [darkTheme, setDarkTheme] = useState(
+        localStorage.getItem("theme") === "true"
+    );
+    const [userName, setUserName] = useState("");
 
     return (
         <div className={darkTheme ? "App dark" : "App"}>
