86a07523a2a702a0bf1d8d5e42bd3d82565520d8
g322h <g2203h@gmail.com>
2022-05-13 00:19:05 +0300

    added styles to grammar page

--- src/pages/Grammar/Grammar.js
+++ src/pages/Grammar/Grammar.js
@@ -1,26 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { simples } from "../../shared/projectData";
-import { GrammarSimple } from "./components/GrammarSimple";
 import "./Grammar.css";
+import grammarBg from "../../assets/images/grammar/grammar-banner.png";
+import { GrammarTest } from "./components/GrammarTest";
 
 export const Grammar = () => {
-    const grammarList = simples.map((item) => {
-        return (
-            <GrammarSimple
-                key={item.id}
-                title={item.title}
-                text1={item.text1}
-                subtitle={item.subtitle}
-                text2={item.text2}
-                rightAnswers={item.rigthAnswers}
-                tasks={item.tasks}
-            />
-        );
-    });
+    const initialState = {
+        simple1: false,
+        simple2: false,
+        simple3: false,
+        simple4: false,
+        simple5: false,
+    };
+    const [show, setShow] = useState(initialState);
     return (
         <div className="grammar">
             <div className="container">
-                <div className="grammar__inner">{grammarList}</div>
+                <div className="listening__nav">
+                    <div className="listening__nav-home">
+                        <Link to={"/"}>Home</Link>
+                    </div>
+                    <div className="listening__nav-next">
+                        <Link to={"/grammar"}>Grammar</Link>
+                    </div>
+                </div>
+                <div className="grammar__inner">
+                    <div className="grammar__banner">
+                        <h1 className="grammar__title">Grammar</h1>
+                        <img src={grammarBg} alt="" />
+                    </div>
+                    <div className="grammar__simples">
+                        <div className="grammar__tasks">
+                            {simples.map((item) => (
+                                <div
+                                    onClick={() =>
+                                        setShow({
+                                            ...initialState,
+                                            [`simple${item.id}`]: true,
+                                        })
+                                    }
+                                    key={item.id}
+                                    className="grammar__task"
+                                >
+                                    {item.title}
+                                </div>
+                            ))}
+                        </div>
+                        <div className="grammar__text">
+                            {simples.map(
+                                (item) =>
+                                    show[`simple${item.id}`] && (
+                                        <>
+                                            <div className="grammar__task-text">
+                                                {item.text1}
+                                            </div>
+                                            <p className="grammar__task-subtitle">
+                                                {item.subtitle}
+                                            </p>
+                                            <div className="grammar__task-text">
+                                                {item.text2}
+                                            </div>
+                                        </>
+                                    )
+                            )}
+                        </div>
+                    </div>
+                    <div className="grammar__test">
+                        {simples.map(
+                            (item) =>
+                                show[`simple${item.id}`] && (
+                                    <GrammarTest
+                                        rightAnswers={item.rigthAnswers}
+                                        tasks={item.tasks}
+                                    />
+                                )
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
     );
